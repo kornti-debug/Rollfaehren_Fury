a848fb9dc6e4c3f935a72b7f2463d3abefb8a563
Philip <philip.mayrhofer@yahoo.de>
2026-06-25 16:00:18 +0200

    feat: remove unused function

--- Assets/Scripts/Weapons/WeaponSystem.cs
+++ Assets/Scripts/Weapons/WeaponSystem.cs
@@ -203,12 +203,6 @@ namespace RollfaehrenFury.Prototype
                 weapons[i]?.EnableReloadDamageBuff(multiplier, duration);
             }
         }
-        
-        /// <summary>Augment hook: Enables AoE explosive damage on the currently active weapon.</summary>
-        public void EnableExplosiveShotsToActive(float radius, float damageMultiplier = 1f)
-        {
-            ActiveWeapon?.EnableExplosiveShots(radius, damageMultiplier);
-        }
 
         /// <summary>Augment hook: Enables AoE explosive damage on all weapons.</summary>
         public void EnableExplosiveShotsOnAll(float radius, float damageMultiplier = 1f)
